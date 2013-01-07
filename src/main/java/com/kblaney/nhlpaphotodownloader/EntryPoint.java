9cae2cc9da6a97f298df1ec9359ed16ae4ba5f19
Kyle Blaney <blaney.kyle@gmail.com>
2013-01-06 19:46:29 -0500

    Do not download photo if output file already exists.

--- src/main/java/com/kblaney/nhlpaphotodownloader/EntryPoint.java
+++ src/main/java/com/kblaney/nhlpaphotodownloader/EntryPoint.java
@@ -1,14 +1,18 @@
 package com.kblaney.nhlpaphotodownloader;
 
+import com.google.common.base.Function;
+import java.io.File;
 import java.io.FileReader;
 import java.io.Reader;
 import java.util.List;
 
 public final class EntryPoint
 {
-  private static final String INPUT_FILE_SPEC = "C:/location/of/player-list.csv.txt";
-  private static final PlayerPhotoDownloader PLAYER_PHOTO_DOWNLOADER = new PlayerPhotoDownloaderImpl(
-        "C:/location/of/output/folder/in/which/to/put/photos");
+  private static final String INPUT_FILE_SPEC = "C:/Temp/nhlpa-photos-download-list/players.csv.txt";
+  private static final String OUTPUT_FOLDER_SPEC = "C:/temp/nhlpa-photos";
+  private static final PlayerPhotoDownloader PLAYER_PHOTO_DOWNLOADER = new PlayerPhotoDownloaderImpl(OUTPUT_FOLDER_SPEC);
+  private static final Function<Player, File> PLAYER_TO_OUTPUT_FILE_FUNCTION = new PlayerToOutputFileFunction(
+        OUTPUT_FOLDER_SPEC);
 
   public static void main(final String[] args) throws Exception
   {
@@ -20,6 +24,7 @@ public final class EntryPoint
       {
         downloadPlayerPhoto(p);
       }
+      System.out.println("Done downloading");
     }
     finally
     {
@@ -29,6 +34,15 @@ public final class EntryPoint
 
   private static void downloadPlayerPhoto(final Player player)
   {
-    PLAYER_PHOTO_DOWNLOADER.download(player);
+    final File outputFile = PLAYER_TO_OUTPUT_FILE_FUNCTION.apply(player);
+    if (outputFile.exists())
+    {
+      System.out.println("Skipping download for " + player.getFullName());
+    }
+    else
+    {
+      System.out.println("Downloading photo for " + player.getFullName());
+      PLAYER_PHOTO_DOWNLOADER.download(player);
+    }
   }
 }
